530e521b4e4e0e89458f09f49bf01f76952aca52
gimseyun <seyoonkim48@gmail.com>
2022-06-08 15:59:49 +0900

    upload

---
+++ src/main/java/com/codestates/deploywithGithubAction/DeployWithGithubActionApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.deploywithGithubAction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployWithGithubActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployWithGithubActionApplication.class, args);
+	}
+
+}
