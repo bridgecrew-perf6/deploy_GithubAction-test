b9c7e15bb9ecba99a5f00a25366b5197c9dafab4
gimseyun <seyoonkim48@gmail.com>
2022-06-20 22:42:13 +0900

    parameter store

--- src/main/java/com/codestates/deploywithGithubAction/controller/HelloController.java
+++ src/main/java/com/codestates/deploywithGithubAction/controller/HelloController.java
@@ -1,14 +1,20 @@
 package com.codestates.deploywithGithubAction.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+  @Value("${bebootcamp}")
+  private String be;
+
+  @Value("${febootcamp}")
+  private String fe;
 
   @GetMapping("/")
   public String hello () {
-    return "Hello World!";
+    return "Hello World!" + be + "and " + fe;
   }
 
   @GetMapping("/bye")
